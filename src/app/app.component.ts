5b25bfa5847b7dbabad0f457846424c6133173bf
donglnp <119558501+donglnp@users.noreply.github.com>
2023-09-05 11:56:19 +0700

    implement supabase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SupabaseService } from './supabase.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'supabase-angular';
+
+  constructor(private readonly supabaseService: SupabaseService) {}
+  ngOnInit(): void {
+    // this.onSubmit();
+
+  }
+
+  async onSubmit(): Promise<void> {
+    try {
+      const email = 'huynhdong.dev@gmail.com';
+      const { error } = await this.supabaseService.signIn(email);
+      console.log('aaaa');
+      if (error) throw error;
+      alert('Check your email for the login link!');
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(error.message);
+      }
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
